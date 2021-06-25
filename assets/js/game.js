db094fb62b436d70f2b06d9196049c3beac42523
Pfizz <pfiste80@gmail.com>
2021-06-25 18:28:20 -0400

    for loops

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,15 +3,16 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-// You can also log multipe values at once like this
-console.log(playerName, playerAttack, playerHealth);
-
-var enemyName = "Megatron";
+var enemyNames = ["Megatron", "Starscream", "Unicron"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
-    // Alert players that they are starting the round
+
+
+
+
+var fight = function(enemyName) {
+    // fight function statements
     window.alert("Welcome to Robot Gladiators!");
 
     var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -57,12 +58,15 @@ var fight = function() {
             fight();
         }
     
-
     } else {
         window.alert("You need to choose a valid option. Try again!");
     }
 
+    
+
 
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
