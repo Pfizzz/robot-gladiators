fae8fd6bd9d292d0a0ada8ce6af6935378ad8505
Pfizz <pfiste80@gmail.com>
2021-07-01 23:14:44 -0400

    multiple rounfs

--- assets/js/game.js
+++ assets/js/game.js
@@ -12,10 +12,11 @@ var enemyAttack = 12;
 
 
 var fight = function(enemyName) {
-    // fight function statements
-    window.alert("Welcome to Robot Gladiators!");
+    // repeat and execute as long as the enemy-robot is alive
+    while (enemyHealth > 0) {
 
-    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
+    var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose."
+    );
 
     //if player choose to fight, then fight
     if (promptFight === "fight" || promptFight === "FIGHT") {
@@ -61,12 +62,11 @@ var fight = function(enemyName) {
     } else {
         window.alert("You need to choose a valid option. Try again!");
     }
-
-    
-
-
+    }
 };
 
 for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
 }
